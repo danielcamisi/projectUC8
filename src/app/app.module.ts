79d7481c2d620968cadee9425f067710fffec76c
DANIEL CAMILLO SILVA <daniel.csilva66@senacsp.edu.br>
2024-11-09 09:49:24 -0300

    first steps and create a structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,53 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {FormsModule} from '@angular/forms'
+
+import { LayoutComponent } from './pages/shared/layout/layout.component';
+import { RegisterComponent } from './pages/view/register/register.component';
+import { LoginComponent } from './pages/view/login/login.component';
+import { DashboardComponent } from './pages/view/dashboard/dashboard.component';
+import { ContentComponent } from './pages/view/content/content.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    RegisterComponent,
+    LoginComponent,
+    DashboardComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
